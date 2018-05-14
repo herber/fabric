2a3048ed52c14ae45f6e55bb6b40bc0e80653301
Tobias Herber <40146720150275@litec.ac.at>
2018-05-14 10:50:12 +0200

    add render script

---
+++ src/render.js
@@ -0,0 +1,63 @@
+const {remote} = require('electron');
+const {Menu, MenuItem} = remote;
+
+if (process.platform !== 'darwin') {
+  require('electron-titlebar');
+}
+
+const template = [
+  {
+    label: 'Edit',
+    submenu: [
+      {role: 'undo'},
+      {role: 'redo'},
+      {type: 'separator'},
+      {role: 'cut'},
+      {role: 'copy'},
+      {role: 'paste'},
+      {role: 'pasteandmatchstyle'},
+      {role: 'delete'},
+      {role: 'selectall'}
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {role: 'reload'},
+      {role: 'forcereload'},
+      {role: 'toggledevtools'},
+      {type: 'separator'},
+      {role: 'resetzoom'},
+      {role: 'zoomin'},
+      {role: 'zoomout'},
+      {type: 'separator'},
+      {role: 'togglefullscreen'}
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      {role: 'minimize'},
+      {role: 'close'}
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click () { require('electron').shell.openExternal('https://fabric.cf') }
+      }
+    ]
+  }
+];
+
+document.querySelector('.menu').onclick = () => {
+  console.log(Menu);
+
+  const menu = Menu.buildFromTemplate(template);
+  menu.popup({
+    x: 20,
+    y: 12
+  })
+};
