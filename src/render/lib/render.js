31448de22d24cd34256399dfe11629f064462425
Tobias Herber <40146720150275@litec.ac.at>
2018-05-16 12:49:49 +0200

    add markdown renderer

---
+++ src/render/lib/render.js
@@ -0,0 +1,19 @@
+const highlight = require('highlight.js');
+const md = require('markdown-it')({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return '<pre class="hljs"><code>' +
+               hljs.highlight(lang, str, true).value +
+               '</code></pre>';
+      } catch (__) {}
+    }
+
+    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+  },
+  linkify: true
+});
+
+module.exports = (raw) => {
+  return md.render(raw);
+};
