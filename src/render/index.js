2553e090c8a364232b72b8e8d2492d04763181b9
Tobias Herber <40146720150275@litec.ac.at>
2018-05-16 12:48:16 +0200

    add preview

--- src/render/index.js
+++ src/render/index.js
@@ -2,10 +2,14 @@ const mitt = require('mitt');
 const titlebar = require('./lib/titlebar');
 const editor = require('./lib/editor');
 const keyboard = require('./lib/keyboard');
+const preview = require('./lib/preview');
 
 const emitter = mitt();
-const state = {};
+const state = {
+  value: ''
+};
 
 titlebar(emitter, state);
 keyboard(emitter, state);
+preview(emitter, state);
 editor(emitter, state);
