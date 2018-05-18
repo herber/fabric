fe76b22253cfb84f5308342fbd0e321b3f6c3e39
Tobias Herber <40146720150275@litec.ac.at>
2018-05-18 18:21:11 +0200

    add syntax highlighting

--- src/render/lib/render.js
+++ src/render/lib/render.js
@@ -1,12 +1,20 @@
 const hljs = require('highlight.js');
 
+hljs.configure({
+  classPrefix: 'code-',
+  usebr: true,
+  tabReplace: '  '
+});
+
 const md = require('markdown-it')({
   highlight: function (str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
-        return '<pre class="hljs"><code>' +
-               hljs.highlight(lang, str, true).value +
-               '</code></pre>';
+        console.log(hljs.highlight(lang, str, true));
+
+        return '<pre style="white-space: pre" class="hljs"><code>' +
+           hljs.highlight(lang, str, true).value.replace(/\n/g, '<br />') +
+           '</code></pre>';
       } catch (__) {}
     }
 
