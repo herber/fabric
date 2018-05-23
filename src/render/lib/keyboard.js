13a719cdd156caf8c7210c70b1b0839d69788185
Tobias Herber <40146720150275@litec.ac.at>
2018-05-23 12:10:25 +0200

    add shortcut for settings

--- src/render/lib/keyboard.js
+++ src/render/lib/keyboard.js
@@ -1,7 +1,17 @@
 module.exports = (emitter) => {
   document.addEventListener('keypress', (e) => {
-    console.log(e.keyCode);
+    // console.log(e.keyCode);
     // console.log(e.ctrlKey);
+
+    if ((e.ctrlKey || e.metaKey) && e.keyCode == 9) {
+      emitter.emit('settings');
+      return;
+    }
+
+    if ((e.ctrlKey || e.metaKey) && e.shiftKey && e.keyCode == 16) {
+      emitter.emit('preview-split');
+      return;
+    }
 
     if ((e.ctrlKey || e.metaKey) && e.keyCode == 14) {
       emitter.emit('editor-new');
@@ -35,11 +45,6 @@ module.exports = (emitter) => {
 
     if ((e.ctrlKey || e.metaKey) && e.keyCode == 2) {
       emitter.emit('export-pdf');
-      return;
-    }
-
-    if ((e.ctrlKey || e.metaKey) && e.keyCode == 9) {
-      emitter.emit('preview-split');
       return;
     }
   }, false);
