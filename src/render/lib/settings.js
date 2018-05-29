818283f023f89f940ea9d01afc4e277b1a339383
Tobias Herber <40146720150275@litec.ac.at>
2018-05-29 08:13:24 +0200

    add styles for checkbox

--- src/render/lib/settings.js
+++ src/render/lib/settings.js
@@ -90,6 +90,72 @@ summary, details {
 details {
   margin: 10px 0px;
 }
+
+.container {
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 22px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Hide the browser's default checkbox */
+.container input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+}
+
+/* Create a custom checkbox */
+.checkmark {
+  border: solid black 1px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 25px;
+  width: 25px;
+  background-color: #fff;
+}
+
+/* On mouse-over, add a grey background color */
+.container:hover input ~ .checkmark {
+  background-color: #fff;
+}
+
+/* When the checkbox is checked, add a blue background */
+.container input:checked ~ .checkmark {
+  background-color: #000;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the checkmark when checked */
+.container input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the checkmark/indicator */
+.container .checkmark:after {
+  left: 9px;
+  top: 5px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
 `;
 
 module.exports = (emitter, state) => {
@@ -121,7 +187,11 @@ module.exports = (emitter, state) => {
         <tr>
           <td>Syntax highlighting</td>
           <td>
-            <input type="checkbox" class="syntaxHighlighting" ${ localStorage.getItem('settings-syntax-highlighting') == 'true' ? 'checked' : '' } onchange=${ () => { emitter.emit('settings-syntax'); } } />
+          <label class="container">
+            <input type="checkbox" class="syntaxHighlighting checkbox" ${ localStorage.getItem('settings-syntax-highlighting') == 'true' ? 'checked' : '' } onchange=${ () => { emitter.emit('settings-syntax'); } } />
+            <span class="checkmark"></span>
+          </label>
+
           </td>
         </tr>
       </table>
