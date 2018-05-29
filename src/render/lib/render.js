de98e781c08e52167c4269321fd2db7aeeeaddbb
Tobias Herber <40146720150275@litec.ac.at>
2018-05-29 08:02:48 +0200

    toggle syntx highlighting state

--- src/render/lib/render.js
+++ src/render/lib/render.js
@@ -7,15 +7,19 @@ hljs.configure({
 
 const md = require('markdown-it')({
   highlight: function (str, lang) {
-    if (lang && hljs.getLanguage(lang)) {
-      try {
-        return '<pre style="white-space: pre" class="code"><code>' +
-           hljs.highlight(lang, str, true).value.replace(/\n/g, '<br />') +
-           '</code></pre>';
-      } catch (__) {}
-    }
+    if (localStorage.getItem('settings-syntax-highlighting') == 'true') {
+      if (lang && hljs.getLanguage(lang)) {
+        try {
+          return '<pre style="white-space: pre" class="code"><code>' +
+          hljs.highlight(lang, str, true).value.replace(/\n/g, '<br />') +
+          '</code></pre>';
+        } catch (__) {}
+      }
 
-    return '<pre class="code"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+      return '<pre class="code"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+    } else {
+      return '<pre class="code"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+    }
   },
   linkify: true
 });
